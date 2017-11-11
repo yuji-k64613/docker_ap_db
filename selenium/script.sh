92a640220925bbc1027994917665cea2bbc37f7c
KonishiYuji <konishiyuji@KonishiYuji-no-MacBook-Air.local>
2017-11-11 16:48:50 +0900

    selenium

--- selenium/script.sh
+++ selenium/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export DISPLAY=:1
+vncserver $DISPLAY -geometry 1024x768
 fvwm &
-vncserver :1 -geometry 1024x768
+
 PATH=$PATH:.
 cd /root
 java -jar selenium-server-standalone-3.6.0.jar &
+
 tail -f /dev/null
--- wildfly/README.txt
+++ wildfly/README.txt
@@ -1,5 +1,6 @@
 http://localhost:8080/myapp/service/main/foo?id=1
 http://localhost:9990
+#admin/password
 
 修正:src/main/resources/META-INF/persistence.xml
 <jta-data-source>java:jboss/datasources/MariaDS</jta-data-source>
